0f95fe9d2fd7a46071e31055818d842713d9550e
Alexandre <alexandre.baumann11@laposte.net>
2023-09-21 23:08:00 +0200

    slide mise en page ok

--- components/Slide.vue
+++ components/Slide.vue
@@ -1,26 +1,24 @@
 <template>
-    <div class="slide">
-        <!-- Titre du slide actuel -->
+    <!-- Titre du slide actuel -->
+    <div class="slideDiv">
         <h2>{{ slides[currentSlideIndex].title }}</h2>
-        <div class="slideDiv">
-            <!-- Bouton précédent -->
-            <button @click="prevSlide" :disabled="currentSlideIndex === 0"><font-awesome-icon
-                    :icon="['fas', 'chevron-left']" /></button>
 
-            <!-- Affichage du slide actuel -->
-            <div v-html="slides[currentSlideIndex].content" class="slideDivContent"></div>
+        <button @click="prevSlide" :disabled="currentSlideIndex === 0" class="previous"><font-awesome-icon
+                :icon="['fas', 'chevron-left']" /></button>
 
-            <!-- Bouton suivant -->
-            <button @click="nextSlide" :disabled="currentSlideIndex === slides.length - 1"><font-awesome-icon
-                    :icon="['fas', 'chevron-right']" /></button>
-        </div>
-        <!-- Points de navigation -->
+        <div v-html="slides[currentSlideIndex].content" class="slideDivContent"></div>
+
+        <button @click="nextSlide" :disabled="currentSlideIndex === slides.length - 1" class="next"><font-awesome-icon
+                :icon="['fas', 'chevron-right']" /></button>
+
         <div class="nav-points">
             <span v-for="(slide, index) in slides" :key="index" :class="{ 'active-point': currentSlideIndex === index }"
                 @click="goToSlide(index)">
                 •
             </span>
         </div>
+        <!-- Points de navigation -->
+
     </div>
 </template>
   
@@ -57,50 +55,92 @@ export default {
   
 <style scoped lang="scss">
 /* Votre CSS ici */
-.slide {
-    &Div {
-        background-color: $dark-blue;
-        color: white;
-        position: relative;
+.slideDiv {
 
-        &Content {
-            height: 300px;
-            padding-right: 30px;
-            padding-left: 30px;
-            overflow-y: auto;
-        }
+    background-color: $blue999;
+    color: $blue50;
+    position: relative;
+    padding-top: 30px;
+    padding-bottom: 10px;
+    border-radius: 10px;
+    box-shadow: 0px 2px 14px rgba(0, 0, 0, 0.5);
 
-        button {
-            height: 100%;
-            position: absolute;
-            padding-right: 10px;
-            padding-left: 10px;
-
-            &:first-child {
-                left: 0; // Positionne le premier bouton à gauche
-            }
-
-            &:last-child {
-                right: 0; // Positionne le dernier bouton à droite
-            }
-
-            svg {
-                top: 50%; // Aligne le bouton à mi-hauteur du conteneur
-            }
-        }
+    a {
+        color: $yellow;
     }
 
+    h2 {
+        text-align: center;
+        margin-top: 0px;
+    }
+
+    &Content {
+        height: 400px;
+        margin: 0px 70px 0px 70px;
+        padding: 0px 10px 0px 10px;
+        overflow-y: auto;
+        background-color: $dark-blue;
+        border-radius: 10px;
+        box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.7);
+
+        &::-webkit-scrollbar {
+            width: 10px;
+
+            &-thumb {
+                background-color: $blue900;
+                border-radius: 6px;
+            }
+
+            &-track {
+                background-color: $dark-blue;
+            }
+        }
+
+        // Pour Firefox
+        scrollbar-color: $blue900 $dark-blue;
+        scrollbar-width: thin;
+
+    }
+
+    button {
+        height: 100%;
+        position: absolute;
+        padding-right: 20px;
+        padding-left: 20px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        top: 0;
+        font-size: 36px;
 
 
+    }
+}
+
+.previous {
+    left: 0; // Positionne le premier bouton à gauche
+}
+
+.next {
+    right: 0; // Positionne le dernier bouton à droite
 }
 
 .nav-points {
     text-align: center;
     cursor: pointer;
+    font-size: 35px;
+    margin-top: 10px;
+    letter-spacing: 5px;
+
+    span {
+        text-shadow: 0 0 4px rgba(0, 0, 0, 1),
+            0 0 8px rgba(0, 0, 0, 1),
+            0 0 12px rgba(0, 0, 0, 01); // Ombres superposées
+    }
 }
 
 .active-point {
-    color: blue;
+    color: $yellow;
 }
 </style>
   
